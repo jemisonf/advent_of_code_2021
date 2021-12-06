6bacd5742736acfefe3e4268cd5b3f8a0b4f82d0
Fischer Jemison <f.g.jemison@gmail.com>
2021-12-05 17:40:41 -0800

    day 4 . . . finally

--- lib/day4.ex
+++ lib/day4.ex
@@ -30,6 +30,54 @@ defmodule Day4 do
     end
   end
 
+  def scan_rows(board) do
+    Enum.reduce(0..4, false, fn col_idx, found ->
+      found || Enum.reduce(0..4, true, fn row_idx, inner_found -> inner_found && board[col_idx][row_idx] == true end)
+    end)
+  end
+
+  def scan_columns(board) do
+    Enum.reduce(0..4, false, fn row_idx, found ->
+      found || Enum.reduce(0..4, true, fn col_idx, inner_found -> inner_found && board[col_idx][row_idx] == true  end)
+    end)
+  end
+
+  def scan_diagonals(board) do
+    Enum.reduce(0..4, true, fn idx, found -> found && board[idx][idx] == true end) ||
+    Enum.reduce(0..4, true, fn idx, found -> found && board[4 - idx][idx] == true end)
+  end
+
+  def update_boards(boards, num) do
+    Enum.map(boards, fn board ->
+        Enum.reduce(0..4, board, fn row_idx, updated_board ->
+          Map.merge(
+            updated_board,
+            %{row_idx =>
+              Enum.reduce(0..4, updated_board[row_idx], fn col_idx, row ->
+                Map.merge(row, %{col_idx => cond do
+                    row[col_idx] == num -> true
+                    true -> row[col_idx]
+                  end})
+              end)
+            }
+          )
+        end)
+      end)
+  end
+
+  def count_unmarked(board) do
+    Enum.reduce(0..4, 0, fn row_idx, sum ->
+            sum + Enum.reduce(0..4, 0, fn col_idx, col_sum ->
+              if board[row_idx][col_idx] == true do
+                col_sum
+              else
+                col_sum + board[row_idx][col_idx]
+              end
+            end)
+        end)
+
+  end
+
   def main(args) do
     options = [switches: [file: :string]]
     {opts, _, _} = OptionParser.parse(args, options)
@@ -47,38 +95,43 @@ defmodule Day4 do
 
     boards = Enum.map(raw_boards, &board/1)
 
-    all_winning_sequences = Enum.map(boards, &winning_sequences/1)
+    Enum.reduce(nums, boards, fn num, boards ->
+      updated_boards = update_boards(boards, num)
 
-    {idx, _} = List.foldl(nums, {0, all_winning_sequences}, fn num, {score, sequences} ->
-      if score != 0 do
-        {score, sequences}
-      else
-        updated_sequences = Enum.map(sequences, fn sequence_list ->
-          Enum.map(sequence_list, fn sequence -> MapSet.delete(sequence, num) end)
-        end)
+      board = Enum.find(updated_boards, nil, fn board ->
+        scan_columns(board) || scan_rows(board) || scan_diagonals(board)
+      end)
 
-        winning_sequences = Enum.find(updated_sequences, nil, fn sequences ->
-          Enum.any?(sequences, fn sequence -> MapSet.size(sequence) == 0 end)
-        end)
+      IO.inspect(num, label: "Num")
+      if board != nil do
+        sum = count_unmarked(board)
+        IO.inspect(sum, label: "Sum")
 
-        if winning_sequences == nil do
-          {0, updated_sequences}
-        else
-          unmarked_values = MapSet.to_list(MapSet.new(Enum.flat_map(winning_sequences, &MapSet.to_list/1)))
-
-          IO.inspect(unmarked_values)
-
-          sum = Enum.reduce(unmarked_values, 0, fn num, acc -> num + acc end)
-
-          IO.inspect(sum, label: "Sum")
-          IO.inspect(num, label: "num")
-
-          {sum * num, updated_sequences}
-        end
+        IO.inspect(sum * num, label: "Result")
       end
+
+      updated_boards
     end)
 
-    IO.inspect(idx, label: "Winning Score")
+    IO.puts("---- part 2")
 
+    Enum.reduce(nums, boards, fn num, boards ->
+      updated_boards = update_boards(boards, num)
+
+      board = Enum.find(updated_boards, nil, fn board ->
+        scan_columns(board) || scan_rows(board) || scan_diagonals(board)
+      end)
+
+      if board != nil && Enum.count(updated_boards) == 1 do
+        sum = count_unmarked(board)
+        IO.inspect(sum, label: "Sum")
+
+        IO.inspect(sum * num, label: "Result")
+      end
+
+      Enum.filter(updated_boards, fn b ->
+        !(scan_columns(b) || scan_rows(b) || scan_diagonals(b))
+      end)
+    end)
   end
 end
